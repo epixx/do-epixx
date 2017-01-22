fc55eca5c3954c38f764a71eedcbac0ffba3d486
Алексей <mr.dojdev@gmail.com>
2017-01-22 21:05:54 +0300

    select style

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -3,23 +3,5 @@
 
 
 jQuery(document).ready(function($){
-
-
+  $("select").selectpicker();
 });
-
-'use strict';
-
-yepnope({
-  test: window.jQuery && window.jQuery.fn.button && window.jQuery.fn.selectpicker,
-  load: [
-    '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js',
-    '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js',
-    '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css',
-    '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.js',
-    '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.css'
-  ],
-  complete: function(){
-    $("select").selectpicker();
-  }
-});
-
