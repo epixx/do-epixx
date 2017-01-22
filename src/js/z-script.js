2bc27581c54fab1554184e5dba7097f18488f2c5
CKutsenko <kate040491@mail.ru>
2017-01-22 20:24:13 +0300

    я на грани суицида (гребанный select)

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -4,52 +4,22 @@
 
 jQuery(document).ready(function($){
 
-(function( $ ) {
-  $.fn.selectbox = function() {
 
-    // начальные параметры
-    // задаем стандартную высоту div'a.
-    var selectDefaultHeight = $('#selectBox').height();
-    // угол поворота изображения в div'e
-    var rotateDefault = "rotate(0deg)";
+});
 
-        // после нажатия кнопки срабатывает функция, в которой
-        // вычисляется исходная высота нашего div'a.
-        // очень удобно для сравнения с входящими параметрами (то, что задается в начале скрипта)
-        $('#selectBox > p.valueTag').click(function() {
-          // вычисление высоты объекта методом height()
-          var currentHeight = $('#selectBox').height();
-          // проверка условия на совпадение/не совпадение с заданной высотой вначале,
-          // чтобы понять. что делать дальше.
-          if (currentHeight < 100 || currentHeight == selectDefaultHeight) {
-              // если высота блока не менялась и равна высоте, заданной по умолчанию,
-              // тогда мы открываем список и выбираем нужный элемент.
-              $('#selectBox').height("150px");  // «точка остановки анимации»
-              // здесь стилизуем нашу стрелку и делаем анимацию средствами CSS3
-              $('img.arrow').css({borderRadius: "1000px", transition: ".2s", transform: "rotate(180deg)"});
-          }
+'use strict';
 
-
-         // иначе если список развернут (высота больше или равна 250 пикселям),
-         // то при нажатии на абзац с классом valueTag, сворачиваем наш список и
-         // и присваиваем блоку первоначальную высоту + поворот стрелки в начальное положение
-          if (currentHeight >= 150) {
-            $('#selectBox').height(selectDefaultHeight);
-            $('img.arrow').css({transform: rotateDefault});
-          }
-      });
-
-     // так же сворачиваем список при выборе нужного элемента
-     // и меняем текст абзаца на текст элемента в списке
-      $('li.option').click(function() {
-        $('#selectBox').height(selectDefaultHeight);
-       $('img.arrow').css({transform: rotateDefault});
-        $('p.valueTag').text($(this).text());
-      });
-  };
-})( jQuery );
-
-$('selector').selectbox();
-
+yepnope({
+  test: window.jQuery && window.jQuery.fn.button && window.jQuery.fn.selectpicker,
+  load: [
+    '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css',
+    '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.js',
+    '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.css'
+  ],
+  complete: function(){
+    $("select").selectpicker();
+  }
 });
 
