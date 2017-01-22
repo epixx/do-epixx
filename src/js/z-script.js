a7d08a51de8e5133c5aeaf3c5177ce20cde453c4
CKutsenko <kate040491@mail.ru>
2017-01-22 19:51:58 +0300

    долбанный select

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -2,244 +2,54 @@
 // window.slick =              require('./vendor/bower/slick');
 
 
-jQuery(document).ready(function($){  
+jQuery(document).ready(function($){
 
-  /* галерея "специалисты" */
-  $('.staff__gallery').slick({
-    infinite: true,
-    dots: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: '0',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+(function( $ ) {
+  $.fn.selectbox = function() {
 
-  $('.hamburger').click(function(e) { 
-      e.preventDefault();
-      $(this).closest('.header').find('.nav').toggle();
-  });
+    // начальные параметры
+    // задаем стандартную высоту div'a.
+    var selectDefaultHeight = $('#selectBox').height();
+    // угол поворота изображения в div'e
+    var rotateDefault = "rotate(0deg)";
 
-  /* business description */
-  $('.business-description__link').click(function(e){
-      e.preventDefault();
-      $(this).closest('.business-description__item').toggleClass('business-description__item--active');
-  }); 
-
-  /* reviews masonry */
-  $('.reviews__articles').masonry({
-    columnWidth: '.review-item',
-    itemSelector: '.review-item'
-  });
-
-  /* Form in modal window */
-  $('*[data-form]').click( function(e){
-    e.preventDefault(); 
-    var suffix = $(this).data('form'); 
-    $('body').css({'overflow':'hidden'});   
-    $('.overlay').show();
-    var formClass = '.form--' + suffix;
-    $('.overlay').find(formClass).fadeIn();
-  });
-  /* Close modal window */
-  $('.overlay__bg, .overlay__close').click( function(e){ 
-    e.preventDefault();
-    $('body').css({'overflow':'auto'});
-    $(this).closest('.overlay').find('.form').fadeOut();
-    $(this).closest('.overlay').fadeOut(400);
-  }); 
-
-  /* question sort - branch */
-  $('.questions-controls__sort-link').click(function(e) { 
-    e.preventDefault();  
-    $(this).closest('.questions-controls__sort').find('.sort').toggle();
-    $(this).toggleClass('questions-controls__sort-link--active');
-  });
-  $('.sort').mouseleave(function(){
-    $(this).fadeOut();
-    $(this).closest('.questions-controls__sort').find('.questions-controls__sort-link').removeClass('questions-controls__sort-link--active');
-  });
-  $('.sort__link').on('click', function(){
-    $(this).closest('.questions-controls__sort').find('.questions-controls__sort-link').removeClass('questions-controls__sort-link--active');
-    $(this).closest('.questions-controls__sort').find('.questions-controls__sort-link').html($(this).html());
-    $('.sort').hide(); 
-    return false;
-  });
-
-  /* Form offers appearing */
-  var offerYourIdea = $('.offer-item--your-idea')
-  $(offerYourIdea).find('.btn').click( function(e){
-    e.preventDefault();
-    $(offerYourIdea).addClass('offer-item--your-idea-form');
-    $(offerYourIdea).find('.offer-item__inner').hide();
-    $(offerYourIdea).find('.form--offers').show();
-    $(offerYourIdea).find('.btn').removeClass('btn--red').addClass('btn--blue');
-  });
-  var offerSelection = $('.offer-item--selection')
-  $(offerSelection).find('.btn').click( function(e){
-    e.preventDefault();
-    $(offerSelection).addClass('offer-item--selection-form');
-    $(offerSelection).find('.offer-item__inner').hide();
-    $(offerSelection).find('.form--offers').show();
-  });
-
-  /* Marks close */
-  $('.marks__close').click( function(e){
-    e.preventDefault();
-    $(this).closest('.marks__item').remove();
-  });
-
-  /* Search */
-  $('.search__icon').click(function(e) { 
-      e.preventDefault();
-      $(this).closest('.search').find('.form').toggle();
-  });
-
-  /* service textblock slideToggle */
-  $('.service__link').click(function(e){
-      e.preventDefault();
-      $(this).closest('.service__item').toggleClass('service__item--active');
-      $(this).closest('.service__item').find('.service__text').slideToggle();
-  }); 
+        // после нажатия кнопки срабатывает функция, в которой
+        // вычисляется исходная высота нашего div'a.
+        // очень удобно для сравнения с входящими параметрами (то, что задается в начале скрипта)
+        $('#selectBox > p.valueTag').click(function() {
+          // вычисление высоты объекта методом height()
+          var currentHeight = $('#selectBox').height();
+          // проверка условия на совпадение/не совпадение с заданной высотой вначале,
+          // чтобы понять. что делать дальше.
+          if (currentHeight < 100 || currentHeight == selectDefaultHeight) {
+              // если высота блока не менялась и равна высоте, заданной по умолчанию,
+              // тогда мы открываем список и выбираем нужный элемент.
+              $('#selectBox').height("150px");  // «точка остановки анимации»
+              // здесь стилизуем нашу стрелку и делаем анимацию средствами CSS3
+              $('img.arrow').css({borderRadius: "1000px", transition: ".2s", transform: "rotate(180deg)"});
+          }
 
 
-  /* Sticky and absolute .service-stages block */
-  var serviceStages = $('.service-stages');
-  var serviceStagesTop = serviceStages.offset().top;
-  var serviceStagesLeft = serviceStages.offset().left;
-  var serviceStagesWidth = serviceStages.width();
-  var serviceStagesOuterHeight = serviceStages.outerHeight(true);
+         // иначе если список развернут (высота больше или равна 250 пикселям),
+         // то при нажатии на абзац с классом valueTag, сворачиваем наш список и
+         // и присваиваем блоку первоначальную высоту + поворот стрелки в начальное положение
+          if (currentHeight >= 150) {
+            $('#selectBox').height(selectDefaultHeight);
+            $('img.arrow').css({transform: rotateDefault});
+          }
+      });
 
-  var serviceStagesBottom;
-  var serviceStagesAbsolute;
-  // console.log(serviceStagesTop) //376
-  // console.log($('.services').outerHeight(true)) //2266
-  // console.log($('.services').offset().top) //90  
-  // console.log(serviceStagesBottom) //2356
-  // console.log(serviceStagesAbsolute) //1930
+     // так же сворачиваем список при выборе нужного элемента
+     // и меняем текст абзаца на текст элемента в списке
+      $('li.option').click(function() {
+        $('#selectBox').height(selectDefaultHeight);
+       $('img.arrow').css({transform: rotateDefault});
+        $('p.valueTag').text($(this).text());
+      });
+  };
+})( jQuery );
 
-  $(window).scroll(function () {
-    if ($(window).width() > 1024) {
-      /* изменение этапа по третям высоты блока контента */
-      if ($(this).scrollTop() >= (serviceStagesTop + $('.services__sidebar').outerHeight(true)*2/3 )) {
-        $('.service-stage-item').hide();
-        $('.service-stage-item--3').show();
-      } else if ($(this).scrollTop() >= (serviceStagesTop + $('.services__sidebar').outerHeight(true)/3 )) {
-        $('.service-stage-item').hide();
-        $('.service-stage-item--2').show();
-      } else {
-        $('.service-stage-item').hide();
-        $('.service-stage-item--1').show();
-      }
-      /* корректная перебивка значения высоты меняюшегося блока */
-      serviceStages = $('.service-stages');
-      serviceStagesOuterHeight = serviceStages.outerHeight(true);
-      serviceStagesBottom = $('.services').offset().top + $('.services').outerHeight(true);
-      serviceStagesAbsolute = serviceStagesBottom - serviceStagesOuterHeight;
-
-      /* service-stages - relative/fixed/absolute */
-      if ($(this).scrollTop() >= serviceStagesAbsolute ) {
-        $(serviceStages).removeClass('service-stages--sticky');
-        $(serviceStages).addClass('service-stages--absolute');
-        $(serviceStages).css({"width":serviceStagesWidth, "left":"10px"});
-      } else if ($(this).scrollTop() >= serviceStagesTop ) {
-        $(serviceStages).removeClass('service-stages--relative service-stages--absolute');
-        $(serviceStages).addClass('service-stages--sticky');
-        $(serviceStages).css({"width":serviceStagesWidth, "left":serviceStagesLeft});
-      } else {
-        $(serviceStages).removeClass('service-stages--sticky');
-        $(serviceStages).addClass('service-stages--relative');
-        $(serviceStages).css({"width":'auto', "left":"0"});
-      }
-    }
-  });
-
-  /* hamburger */
-  // $('.hamburger').click(function(e) { 
-  //     e.preventDefault();    
-  //     $(this).closest('.header').find('.header__nav').slideToggle();
-  //     $(this).closest('.header').find('.header__services').slideToggle();
-  // });
-  // $(window).resize(function(){
-  //   if ($(window).width() > 768) {
-  //     $('.header__nav').show();
-  //     $('.header__services').show();
-  //   } else {
-  //     $('.header__nav').hide();
-  //     $('.header__services').hide();
-  //   }
-  // });
-
-
-
-
-
-
-
-
-  // /* плавный скролл наверх */
-  // $('.up').click(function () {
-  //   $("html, body").animate({
-  //     scrollTop: 0
-  //   }, 600);
-  //   return false;
-  // });
-
-  // /* sticky menu */
-  // var headerInner = $('.header__inner');
-  // var headerNav = $('.header__nav'); 
-  // var navList = $('.nav__list');
-  // var navItemLogo = $(navList).find('.nav__item--logo');
-  // var navItemBtn = $(navList).find('.nav__item--btn');      
-
-  // $(window).scroll(function () {
-  //   if ($(window).width() > 768) {
-  //     if ($(this).scrollTop() >= headerInner.height() ) {
-  //       $(headerNav).addClass('header__nav--sticky');
-  //       $(navList).addClass('nav__list--sticky');
-  //       $(navItemLogo).show();
-  //       $(navItemBtn).hide();
-  //     } else {
-  //       $(headerNav).removeClass('header__nav--sticky');
-  //       $(navList).removeClass('nav__list--sticky');
-  //       $(navItemLogo).hide();
-  //       $(navItemBtn).show();
-  //     }
-  //   }
-  // });
-
-  /* 404 - sticky footer */
-  // var footerHeight = $('.footer').outerHeight();
-  // if ($(window).height() >= $('body').height()){
-  //   $('body').addClass('body--full-screen');
-  //   $('body').css('padding-bottom', footerHeight+'px');
-  //   $('.footer').addClass('footer--stuck-to-bottom');
-  // }
-
-
-
-
-    /* Map */
-    // var map = new GMaps({
-    //     el: '.contacts__map',
-    //     lat: 59.971198,
-    //     lng: 30.315121,
-    //     scrollwheel: false
-    // });
-    // map.drawOverlay({
-    //     lat: 59.971198,
-    //     lng: 30.315121,
-    //     content: '<div class="pin"></div>'
-    // });
+$('selector').selectbox();
 
 });
 
