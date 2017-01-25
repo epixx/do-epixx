5981e95d1ad59875170450e6223a59048ce90392
CKutsenko <kate040491@mail.ru>
2017-01-25 22:08:12 +0300

    табчики для двух окошек задания

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -16,4 +16,46 @@ jQuery(document).ready(function($){
      });
   });
 
+  //Табчики для окошка Результат-Теория-Подсказка
+
+  $('.result__item').on('click', function(e){
+  e.preventDefault();
+
+  $(this)
+    .closest('.result__list')
+    .find('.result__link.result__link--active')
+    .removeClass('result__link--active');
+  $(this)
+   .closest('.result__link')
+   .addClass('result__link--active');
+
+  $(this)
+   .closest('.result')
+   .find('.result__content-wrapper.result__content-wrapper--active')
+   .removeClass('result__content-wrapper--active');
+  link_href = $(this).attr('href');
+  $(link_href).addClass('result__content-wrapper--active');
+  });
+
+    //Табчики для окошка HTML-CSS-JS
+
+  $('.code__item').on('click', function(e){
+  e.preventDefault();
+
+  $(this)
+    .closest('.code__list')
+    .find('.code__link.code__link--active')
+    .removeClass('code__link--active');
+  $(this)
+   .closest('.code__link')
+   .addClass('code__link--active');
+
+  $(this)
+   .closest('.code')
+   .find('.code__content-wrapper.code__content-wrapper--active')
+   .removeClass('code__content-wrapper--active');
+  link_href = $(this).attr('href');
+  $(link_href).addClass('code__content-wrapper--active');
+  });
+
 });
